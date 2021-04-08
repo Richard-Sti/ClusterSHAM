8c49040c9d13c11cdf7fb9e3fc0573a200c47f02
Richard <richard.stiskalek@protonmail.com>
2021-04-08 14:59:06 +0200

    Remove base

--- clustersham/mocks/__init__.py
+++ clustersham/mocks/__init__.py
@@ -13,9 +13,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-"""Tools to work with mock galaxy catalogues."""
-
-
 from .correlators import Correlator
 from .abundance_match import AbundanceMatch
 from .proxy import (VirialMassProxy, VirialVelocityProxy, PeakRedshiftProxy,
